72b033b30cf2ec7fe9558a68eb4fc425f6744fe1
satoshiTajima005 <satoshi.tajima@gmail.com>
2021-04-26 13:02:39 +0900

    first commit

---
+++ js.js
@@ -0,0 +1,585 @@
+const itemlist = [{
+    itemcode: "5MVS336SF001-",
+    itemname: "ＥＢ１０５８　ＳＨＥＥＴ　ＤＵＳＴ　ＳＵＢ",
+    kana: "",
+    tokui: "(13572)京セラ"
+  },
+  {
+    itemcode: "5MVS111EF446-",
+    itemname: "ＤＢ６２　ＴＡＰＥ　ＣＯＶＥＲ　ＡＸＩＳ　Ｌ",
+    kana: "",
+    tokui: "(13572)京セラ"
+  },
+  {
+    itemcode: "5MVS412EF020-",
+    itemname: "ＥＢ１０６５　ＴＡＰＥ　ＢＵＴＴＯＮ　ＶＯＬ",
+    kana: "",
+    tokui: "(13572)京セラ"
+  },
+  {
+    itemcode: "5MVS216EF012-",
+    itemname: "ＳＰＡＣＥＲ　ＵＳＢ",
+    kana: "",
+    tokui: "(13572)京セラ"
+  },
+  {
+    itemcode: "5MVS111EF446-",
+    itemname: "ＤＢ６２　ＴＡＰＥ　ＣＯＶＥＲ　ＡＸＩＳ　Ｌ",
+    kana: "",
+    tokui: "(13572)京セラ"
+  },
+  {
+    itemcode: "5MVS216EF012-",
+    itemname: "ＳＰＡＣＥＲ　ＵＳＢ",
+    kana: "",
+    tokui: "(13572)京セラ"
+  },
+  {
+    itemcode: "5MVS111EF446-",
+    itemname: "ＤＢ６２　ＴＡＰＥ　ＣＯＶＥＲ　ＡＸＩＳ　Ｌ",
+    kana: "",
+    tokui: "(13572)京セラ"
+  },
+  {
+    itemcode: "5MVS216EF012-",
+    itemname: "ＳＰＡＣＥＲ　ＵＳＢ",
+    kana: "",
+    tokui: "(13572)京セラ"
+  },
+  {
+    itemcode: "5MVS111EF446-",
+    itemname: "ＤＢ６２　ＴＡＰＥ　ＣＯＶＥＲ　ＡＸＩＳ　Ｌ",
+    kana: "",
+    tokui: "(13572)京セラ"
+  },
+  {
+    itemcode: "5MVS216EF012-",
+    itemname: "ＳＰＡＣＥＲ　ＵＳＢ",
+    kana: "",
+    tokui: "(13572)京セラ"
+  },
+  {
+    itemcode: "5MVS111EF446-",
+    itemname: "ＤＢ６２　ＴＡＰＥ　ＣＯＶＥＲ　ＡＸＩＳ　Ｌ",
+    kana: "",
+    tokui: "(13572)京セラ"
+  },
+  {
+    itemcode: "5MVS216EF012-",
+    itemname: "ＳＰＡＣＥＲ　ＵＳＢ",
+    kana: "",
+    tokui: "(13572)京セラ"
+  },
+  {
+    itemcode: "5MVS111EF446-",
+    itemname: "ＤＢ６２　ＴＡＰＥ　ＣＯＶＥＲ　ＡＸＩＳ　Ｌ",
+    kana: "",
+    tokui: "(13572)京セラ"
+  },
+  {
+    itemcode: "5MVS216EF012-",
+    itemname: "ＳＰＡＣＥＲ　ＵＳＢ",
+    kana: "",
+    tokui: "(13572)京セラ"
+  },
+  {
+    itemcode: "5MVS111EF446-",
+    itemname: "ＤＢ６２　ＴＡＰＥ　ＣＯＶＥＲ　ＡＸＩＳ　Ｌ",
+    kana: "",
+    tokui: "(13572)京セラ"
+  },
+  {
+    itemcode: "5MVS216EF012-",
+    itemname: "ＳＰＡＣＥＲ　ＵＳＢ",
+    kana: "",
+    tokui: "(13572)京セラ"
+  },
+  {
+    itemcode: "5MVS111EF446-",
+    itemname: "ＤＢ６２　ＴＡＰＥ　ＣＯＶＥＲ　ＡＸＩＳ　Ｌ",
+    kana: "",
+    tokui: "(13572)京セラ"
+  },
+  {
+    itemcode: "5MVS216EF012-",
+    itemname: "ＳＰＡＣＥＲ　ＵＳＢ",
+    kana: "",
+    tokui: "(13572)京セラ"
+  },
+  {
+    itemcode: "5MVS111EF446-",
+    itemname: "ＤＢ６２　ＴＡＰＥ　ＣＯＶＥＲ　ＡＸＩＳ　Ｌ",
+    kana: "",
+    tokui: "(13572)京セラ"
+  },
+  {
+    itemcode: "5MVS216EF012-",
+    itemname: "ＳＰＡＣＥＲ　ＵＳＢ",
+    kana: "",
+    tokui: "(13572)京セラ"
+  },
+  {
+    itemcode: "5MVS111EF446-",
+    itemname: "ＤＢ６２　ＴＡＰＥ　ＣＯＶＥＲ　ＡＸＩＳ　Ｌ",
+    kana: "",
+    tokui: "(13572)京セラ"
+  },
+  {
+    itemcode: "5MVS216EF012-",
+    itemname: "ＳＰＡＣＥＲ　ＵＳＢ",
+    kana: "",
+    tokui: "(13572)京セラ"
+  },
+  {
+    itemcode: "5MVS111EF446-",
+    itemname: "ＤＢ６２　ＴＡＰＥ　ＣＯＶＥＲ　ＡＸＩＳ　Ｌ",
+    kana: "",
+    tokui: "(13572)京セラ"
+  },
+]
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    editor: ClassicEditor,
+    editorData: '<p>自由入力テキストエリア</p><h2><a href="https://ckeditor.com/ckeditor-5/demo/"><strong>エディターデモページ</strong></a></h2><figure class="image image-style-side"><img src="https://placehold.jp/640x480.png"><figcaption>画像に対するキャプションが入れられます</figcaption></figure><p>&nbsp;</p><p>画像を横にずらして、説明文を入れられます。</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>',
+    editorConfig: {
+      language: 'ja'
+    },
+    flash: false,
+    current_view: 'search',
+    result: [],
+    current_line: 0,
+    proc: [{
+        line: 10,
+        SEQ: 1,
+        GRP: "G1",
+        ORD: 1,
+        place: "00021 新光１Ｇ",
+        next: 30,
+        detail: [{
+            "workPlace": 6,
+            "procName": "受入",
+            "detail": "資材確認"
+          },
+          {
+            "workPlace": 21,
+            "procName": "段取り",
+            "detail": "スリット、PET・X-30・黒・0.05t、70巾※後貼用"
+          },
+          {
+            "workPlace": 21,
+            "procName": "段取り",
+            "detail": "スリット、OPPセパ・40RL-01、70巾"
+          },
+          {
+            "workPlace": 21,
+            "procName": "段取り",
+            "detail": "突切、DIC#8616UJ黒-50(N)、68巾"
+          },
+          {
+            "workPlace": 21,
+            "procName": "段取り",
+            "detail": "スリット、PETセパ・75X1-J0、90巾"
+          },
+          {
+            "workPlace": 21,
+            "procName": "貼り合わせ",
+            "detail": "上記(J0+WF)貼合※セパ面センター貼"
+          },
+          {
+            "workPlace": 21,
+            "procName": "貼り合わせ",
+            "detail": "上記{(J0+WF)+OPPセパ}貼合※WFセパ剥しセパ面貼"
+          },
+          {
+            "workPlace": 21,
+            "procName": "抜き",
+            "detail": "半抜(ガイド総抜)、上記貼品、OPPセパ刃入、送り211.5㎜"
+          },
+          {
+            "workPlace": 21,
+            "procName": "貼り込み",
+            "detail": "中カス上げ・外周セパ上げ+上記PET・黒、ロール内貼込"
+          },
+          {
+            "workPlace": "",
+            "procName": "",
+            "detail": "シートカット(1P=5ヶ)※ダンカットテープ、かまし材(PET・S・0.025t)使用"
+          }
+        ]
+      },
+      {
+        line: 20,
+        SEQ: 1,
+        GRP: "G2",
+        ORD: 1,
+        place: "00021 新光１Ｇ",
+        next: 30,
+        detail: [{
+            "workPlace": 21,
+            "procName": "段取り",
+            "detail": "スリット、弱粘着・4M-KTF-75M、90巾"
+          },
+          {
+            "workPlace": 21,
+            "procName": "ウラスリ入れ",
+            "detail": "上記弱粘着、センター振分67巾、セパ面セパのみカット"
+          },
+          {
+            "workPlace": 21,
+            "procName": "段取り",
+            "detail": "スリット、銅テープ・E-50CU、70巾"
+          },
+          {
+            "workPlace": 21,
+            "procName": "貼り合わせ",
+            "detail": "上記(弱粘着+銅テープ)貼合※弱粘着センターセパ剥し銅面貼"
+          },
+          {
+            "workPlace": 21,
+            "procName": "抜き",
+            "detail": "半抜(ガイド総抜)、上記貼品、銅テープセパ面刃入、送り210.5㎜ シートカット(1P=5ヶ)"
+          }
+        ]
+      },
+      {
+        line: 30,
+        SEQ: 2,
+        GRP: "G3",
+        ORD: 1,
+        place: "00024 新光仕上",
+        next: 40,
+        detail: [{
+            "workPlace": 24,
+            "procName": "カス取り",
+            "detail": "上記(弱粘着+銅テープ)貼抜品、中カス取"
+          },
+          {
+            "workPlace": 24,
+            "procName": "貼り合わせ",
+            "detail": "上記{(抜貼品(PET黒含む)+貼抜カス取品(銅箔含む)}貼合≪治具≫"
+          },
+          {
+            "workPlace": "",
+            "procName": "",
+            "detail": "※PET黒面上先入れ、銅テープセパ剥し貼、貼後弱粘着剥す"
+          }
+        ]
+      },
+      {
+        line: 40,
+        SEQ: 2,
+        GRP: "G3",
+        ORD: 2,
+        place: "00022 新光２Ｇ",
+        next: 70,
+        detail: [{
+          "workPlace": 22,
+          "procName": "抜き",
+          "detail": "半抜、上記貼品、銅箔面刃入※ピナダイ・4-90-80共用"
+        }]
+      },
+      {
+        line: 50,
+        SEQ: 2,
+        GRP: "G4",
+        ORD: 1,
+        place: "00021 新光１Ｇ",
+        next: 60,
+        detail: [{
+            "workPlace": 21,
+            "procName": "段取り",
+            "detail": "スリット、ピュアセルECS-250PR・0.4t、40巾※後貼用"
+          },
+          {
+            "workPlace": 21,
+            "procName": "段取り",
+            "detail": "突切、TESA・60261、38巾※後貼用"
+          },
+          {
+            "workPlace": 21,
+            "procName": "段取り",
+            "detail": "スリット、OPPセパ・40RL-01、70巾※後貼用"
+          },
+          {
+            "workPlace": 21,
+            "procName": "段取り",
+            "detail": "スリット、PETテープ・TL-85-25・緑、90巾"
+          },
+          {
+            "workPlace": 21,
+            "procName": "段取り",
+            "detail": "スリット、弱粘着・RB114(20)、90巾"
+          },
+          {
+            "workPlace": 21,
+            "procName": "段取り",
+            "detail": "スリット、PET・S・0.05t、90巾"
+          },
+          {
+            "workPlace": 21,
+            "procName": "貼り合わせ",
+            "detail": "上記(TL緑+弱粘着)貼合※TL緑セパ剥し弱粘着面貼"
+          },
+          {
+            "workPlace": 21,
+            "procName": "貼り合わせ",
+            "detail": "上記{(TL緑+弱粘着)+PET}貼合※弱粘着セパ剥しロール外貼"
+          },
+          {
+            "workPlace": 21,
+            "procName": "抜き",
+            "detail": "PET透明まで刃入(ガイド総抜)、上記貼品(TL緑含む)、PET透明刃入 送り210.5㎜"
+          },
+          {
+            "workPlace": 21,
+            "procName": "貼り込み",
+            "detail": "外周PET透明上げ+上記ピュアセル、四角PETを覆うように、ピュアセル面貼込"
+          },
+          {
+            "workPlace": 21,
+            "procName": "貼り込み",
+            "detail": "上記貼込品+上記WF、保護シート剥し、ピュアセル面に貼込"
+          },
+          {
+            "workPlace": 21,
+            "procName": "貼り込み",
+            "detail": "上記貼込品+上記OPPセパ、WFセパ剥し、セパ面貼込"
+          }
+        ]
+      },
+      {
+        line: 60,
+        SEQ: 2,
+        GRP: "G4",
+        ORD: 2,
+        place: "00022 新光２Ｇ",
+        next: 70,
+        detail: [{
+            "workPlace": 22,
+            "procName": "抜き",
+            "detail": "ピュアセルまで刃入、上記貼品、OPPセパ刃入 送り210.5㎜"
+          },
+          {
+            "workPlace": "",
+            "procName": "",
+            "detail": "シートカット(1P=5ヶ) ※ピナダイ・4-90-80共用"
+          }
+        ],
+      },
+      {
+        line: 70,
+        SEQ: 3,
+        GRP: "G5",
+        ORD: 1,
+        place: "00024 新光仕上",
+        next: 80,
+        detail: [{
+            "workPlace": 24,
+            "procName": "カス取り",
+            "detail": "上記貼抜品(ピュアセル含む)、中、外周カス取"
+          },
+          {
+            "workPlace": 24,
+            "procName": "カス取り",
+            "detail": "上記貼抜品(銅箔含む)、中、両サイドカス取"
+          },
+          {
+            "workPlace": 24,
+            "procName": "貼り合わせ",
+            "detail": "上記{貼抜品(ピュアセル含む)+貼抜品(銅箔含む)}貼合≪治具≫"
+          },
+          {
+            "workPlace": "",
+            "procName": "",
+            "detail": "※銅箔面上先入れ、ピュアセル上セパ剥し貼"
+          }
+        ]
+      },
+      {
+        line: 80,
+        SEQ: 3,
+        GRP: "G5",
+        ORD: 2,
+        place: "00022 新光２Ｇ",
+        next: 90,
+        detail: [{
+            "workPlace": 21,
+            "procName": "抜き",
+            "detail": "半抜、上記貼品、TL緑刃入 ※ピナダイ・4-90-80共用"
+          },
+          {
+            "workPlace": 24,
+            "procName": "包装",
+            "detail": "外周カス取 数確認 仮包装⇒≪イーボス≫へ"
+          }
+        ]
+      },
+      {
+        line: 90,
+        SEQ: 3,
+        GRP: "G5",
+        ORD: 3,
+        place: "30885 イーボス",
+        next: 0,
+        detail: [{
+            "workPlace": 29,
+            "procName": "段取り",
+            "detail": "スリット、茶セパ・70KPS、70巾≪下取依頼書発行≫"
+          },
+          {
+            "workPlace": 30885,
+            "procName": "外作",
+            "detail": "≪後全行程≫ ※セパ貼替等"
+          }
+        ]
+      }
+    ],
+    detail_proc: 'shitadori',
+    shitadori_proc: '#8800CH',
+    hari_proc: 'L1',
+    press_proc: '16',
+    dlg_pic: {
+      isShow: false,
+      current: 0
+    },
+    dlg_mov: {
+      isShow: false,
+      current: 0
+    },
+    dlg_oth: {
+      isShow: false
+    },
+    dlg_pdf: {
+      isShow: false,
+      url: ""
+    },
+    item_pic: {
+      current: 0,
+      src: [
+        "img/Koala.jpg",
+        "img/Lighthouse.jpg",
+        "img/Penguins.jpg",
+        "img/Tulips.jpg",
+        "img/Chrysanthemum.jpg",
+        "img/Desert.jpg",
+        "img/Hydrangeas.jpg",
+        "img/Jellyfish.jpg"
+      ]
+    },
+    dlg_camera: {
+      isShow: false,
+      isEdit: false,
+      picData: ""
+    }
+  },
+  mounted:function(){
+    const me = this;
+    this.$nextTick(function () {
+      if (navigator.mediaDevices){
+        this.video  = document.querySelector("#camera");
+        this.canvas = document.querySelector("#cvs");
+      
+        /** カメラ設定 */
+        const constraints = {
+          audio: false,
+          video: {
+            width: 1280,
+            height: 720,
+            //facingMode: "user"   // フロントカメラを利用する
+            facingMode: "environment"  // リアカメラを利用する場合
+          }
+        };
+      
+        //カメラを<video>と同期
+        const me = this;
+        navigator.mediaDevices.getUserMedia(constraints).then(function(stream){
+          me.video.srcObject = stream;
+          me.video.onloadedmetadata = function(e){
+            me.video.play();
+          };
+        }).catch(function(err){
+          console.log(err.name + ": " + err.message);
+        });
+      }
+
+      Vue.use(CKEditor);
+    });
+  },
+  methods: {
+    search: function () {
+      this.result = itemlist;
+    },
+    click_to_top: function (e) {
+      e.preventDefault();
+      this.current_view = 'search'
+      this.current_line = 0;
+    },
+    run_flash: function(){
+      this.flash = true;
+      let me = this;
+      setTimeout(function () {
+        me.flash = false;
+      }, 3000);
+    },
+    click_to_item: function () {
+      this.current_view = 'item';
+      this.run_flash();
+    },
+    click_to_detail: function (line) {
+      this.current_view = 'detail'
+      this.current_line = line;
+      this.run_flash();
+    },
+    open_dlg_pic: function () {
+      this.dlg_pic.isShow = true;
+    },
+    open_dlg_mov: function () {
+      this.dlg_mov.isShow = true;
+    },
+    open_dlg_oth: function () {
+      this.dlg_oth.isShow = true;
+    },
+    shutter: function(){
+      const ctx = this.canvas.getContext("2d");
+      ctx.drawImage(this.video, 0, 0, this.canvas.width, this.canvas.height);
+      this.dlg_camera.picData = this.canvas.toDataURL("image/png");
+      this.dlg_camera.isEdit = true;
+      /*
+      const image = document.getElementById('editPic');
+      const cropper = new Cropper(image, {
+        aspectRatio: 16 / 9,
+        crop(event) {
+          console.log(event.detail.x);
+          console.log(event.detail.y);
+          console.log(event.detail.width);
+          console.log(event.detail.height);
+          console.log(event.detail.rotate);
+          console.log(event.detail.scaleX);
+          console.log(event.detail.scaleY);
+        },
+      });
+      */
+    },
+    delPic: function(){
+      this.dlg_camera.picData = '';
+      this.dlg_camera.isEdit = false;
+    },
+    startEdit: function(){
+      
+    },
+    addPic: function(){
+      this.item_pic.src.push(this.dlg_camera.picData);
+      this.closeCamera();
+    },
+    closeCamera: function(){
+      this.dlg_camera.picData = '';
+      this.dlg_camera.isEdit = false;
+      this.dlg_camera.isShow = false;
+    },
+    mountCamera: function(){
+
+    }
+  }
+});
